91eb0b6316eafb7f3ec3839237410e1591c312e0
srikar <161401415+SiripuramSrikar@users.noreply.github.com>
2025-04-27 17:57:02 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,8 +16,8 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Bitlinks - Your trusted URL shortener",
-  description: "Bitlinks helps you shorten your Urls easily",
+  title: "Simplelinks - Your trusted URL shortener",
+  description: "Simplelinks helps you shorten your Urls easily",
 };
 
 export default function RootLayout({ children }) {
